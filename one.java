843402cc049c0924e3acdd96aa1f6feb50a3cdbb
ruslANUS69 <152083722+ruslANUS69@users.noreply.github.com>
2023-11-26 21:03:13 +0300

    Update one.java

--- one.java
+++ one.java
@@ -1,21 +1,26 @@
 import java.util.Scanner;
 public class one {
- 	public static void main(String[] args) 
-	throws java.io.IOException {
-		Scanner in = new Scanner(System.in);
-		int kol, i;
- 		System.out.print("Введите количество чисел, которое вы хотите ввести\n");
-		kol = in.nextInt();
-		int[] nums = new int[kol];
-		System.out.println("Введите числа");
-		for(i=0; i<kol; i++) 
-			nums [i] = in.nextInt();
-		System.out.println("\nРезультат:");
-		for (i=0; i<kol; i++) 
-			if (nums [i] == 0 || nums [i] == 1 || nums [i] == 2 || nums [i] == 3
-			 || nums [i] == 5 || nums [i] == 8 || nums [i] == 13) /* последовательность 
-можно до бесконечности писать, напичал пару штук, чтобы показать суть моего решения) */
-			System.out.println("Число " + nums [i] + " принадлежит последовательностти Фибоначи");
-		System.out.println("Если ничего не вывелось значит, простых чисел нет");
-	}
+ public static void main(String[] args) 
+ throws java.io.IOException {
+  Scanner in = new Scanner(System.in);
+  int kol, i;
+  System.out.println("Введите количество чисел, которое вы хотите ввести");
+  kol = in.nextInt();
+  int[] nums = new int[kol];
+  System.out.println("Введите числа");
+  for(i=0; i<nums.length; i++) 
+  nums [i] = in.nextInt();
+  System.out.println("\nРезультат:");
+  for (i=0; i<kol; i++) { 
+   int a = 0, b = 1; // для рассчёта ряда фибоначи
+   if ((nums [i] == a) || (nums [i] == b)) System.out.print(nums [i] + " ");
+   while (b<10000) {
+    a = b;
+    b = b + a;
+    if ((nums [i] == a) || (nums [i] == b)) System.out.print(nums [i]+ " ");
+   }
+  }
+  System.out.print("Эти числа принадлежат последовательностти Фибоначи");
+System.out.println("Если ничего не вывелось значит, простых чисел нет");
+ }
 }
